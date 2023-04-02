23000fca8b252599a370d1fb9fba20627df85964
cms04 <Christian-Morbach@hotmail.com>
2023-04-02 13:46:32 +0200

    Add logs_to_csv script

---
+++ scripts/logs_to_csv.sh
@@ -0,0 +1,23 @@
+  GNU nano 5.4                                                                                        logs_to_csv.sh                                                                                                 
+#!/bin/bash
+
+set -e
+
+if [ $# != "1" ]; then
+    echo "ERROR: you have to specify a filename for the output csv file!"
+    exit 1
+fi
+
+FILENAME=$1
+
+echo 'date;time;ip;port;type;iso_code;country' > $FILENAME
+
+cat tarpit.log | grep -e 'Client' |\
+    awk '{print $3,$4,$8,$9,$10}' |\
+    sed -E "s/\('//g" | sed -E "s/'\,//g" | sed -E "s/\)//g" |\
+    while read s; do
+        echo $s $(geoiplookup $(echo $s | awk '{print $3}') | awk '{print $4}{for(i=5;i<=NF;i++)print $i}' | sed -E "s/\,\n/\;/" | tr '\n' '+')
+    done |\
+    tr ' ' ';' | tr '+' ' ' | sed -E "s/\, /\;/" >> $FILENAME
+
+exit 0
