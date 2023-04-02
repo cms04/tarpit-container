32c475398e8ca65aefc0084c34ef49d75def173f
cms04 <Christian-Morbach@hotmail.com>
2023-04-02 16:05:12 +0200

    Bugfixes

--- scripts/logs_to_csv.sh
+++ scripts/logs_to_csv.sh
@@ -7,9 +7,12 @@ if [ $# != "1" ]; then
     exit 1
 fi
 
+PATH=/tmp/tarpit/
 FILENAME=$1
 
-echo 'date;time;ip;port;type;iso_code;country' > $FILENAME
+mkdir -p $PATH
+
+echo 'date;time;ip;port;type;iso_code;country' > $PATH$FILENAME
 
 cat tarpit.log | grep -e 'Client' |\
     awk '{print $3,$4,$8,$9,$10}' |\
@@ -17,6 +20,6 @@ cat tarpit.log | grep -e 'Client' |\
     while read s; do
         echo $s $(geoiplookup $(echo $s | awk '{print $3}') | awk '{print $4}{for(i=5;i<=NF;i++)print $i}' | sed -E "s/\,\n/\;/" | tr '\n' '+')
     done |\
-    tr ' ' ';' | tr '+' ' ' | sed -E "s/\, /\;/" >> $FILENAME
+    tr ' ' ';' | tr '+' ' ' | sed -E "s/\, /\;/" >> $PATH$FILENAME
 
 exit 0
